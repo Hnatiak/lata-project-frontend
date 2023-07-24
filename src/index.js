9838e5751a4275a46082c8f3ac517882e40a4863
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2023-07-24 15:34:59 +0200

    Пофіксив помилку

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import { persistor, store } from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
-import App from './App';
+import { App } from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
