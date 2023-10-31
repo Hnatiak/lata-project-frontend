5bff0f4e6ab075c9aba72acbd908a457e35155ac
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2023-10-31 12:22:08 +0100

    I fixed mistakes

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/lata-project-frontend">
+        <BrowserRouter>
           <App />
         </BrowserRouter>
       </PersistGate>
