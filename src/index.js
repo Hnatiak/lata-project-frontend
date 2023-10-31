8f138e016d68183250f0cd6606731bd14c2012a8
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2023-10-31 12:16:31 +0100

    I fixed mistakes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { Provider } from 'react-redux';
+// import { persistor, store } from './redux/store';
+// import { BrowserRouter } from 'react-router-dom';
+// import { PersistGate } from 'redux-persist/integration/react';
+// import './styles/modern-normalize.css';
+// import './index.css';
+// import { App } from './components/App';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <PersistGate loading={null} persistor={persistor}>
+//         <BrowserRouter basename="/lata-project-frontend">
+//           <App />
+//         </BrowserRouter>
+//       </PersistGate>
+//     </Provider>
+//   </React.StrictMode>
+// );
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom'; // Зверніть увагу на цей рядок
 import { Provider } from 'react-redux';
 import { persistor, store } from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
