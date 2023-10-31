8f63a24e9290aeb7618c0430ff0e2752a2678a2f
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2023-10-31 12:49:42 +0100

    Виправив помилки і добавив фотографії

--- src/index.js
+++ src/index.js
@@ -1,27 +1,5 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import { Provider } from 'react-redux';
-// import { persistor, store } from './redux/store';
-// import { BrowserRouter } from 'react-router-dom';
-// import { PersistGate } from 'redux-persist/integration/react';
-// import './styles/modern-normalize.css';
-// import './index.css';
-// import { App } from './components/App';
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <Provider store={store}>
-//       <PersistGate loading={null} persistor={persistor}>
-//         <BrowserRouter basename="/lata-project-frontend">
-//           <App />
-//         </BrowserRouter>
-//       </PersistGate>
-//     </Provider>
-//   </React.StrictMode>
-// );
-
 import React from 'react';
-import ReactDOM from 'react-dom'; // Зверніть увагу на цей рядок
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { persistor, store } from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
@@ -34,10 +12,32 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename="/lata-project-frontend">
           <App />
         </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
 );
+
+// import React from 'react';
+// import ReactDOM from 'react-dom'; // Зверніть увагу на цей рядок
+// import { Provider } from 'react-redux';
+// import { persistor, store } from './redux/store';
+// import { BrowserRouter } from 'react-router-dom';
+// import { PersistGate } from 'redux-persist/integration/react';
+// import './styles/modern-normalize.css';
+// import './index.css';
+// import { App } from './components/App';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <PersistGate loading={null} persistor={persistor}>
+//         <BrowserRouter>
+//           <App />
+//         </BrowserRouter>
+//       </PersistGate>
+//     </Provider>
+//   </React.StrictMode>
+// );
